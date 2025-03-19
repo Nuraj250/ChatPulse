89f443ee031878672fada772b76fc01b025e33b3
Nuraj250 <nurajshaminda200@gmail.com>
2025-03-18 21:36:49 -0600

    inint 2

--- app/socket_events.py
+++ app/socket_events.py
@@ -1,6 +1,11 @@
 from flask_socketio import emit, join_room, leave_room
 from app import socketio
 
+def generate_avatar(username):
+    """Generate a unique avatar using Gravatar."""
+    hash_email = hashlib.md5(username.encode()).hexdigest()
+    return f"https://www.gravatar.com/avatar/{hash_email}?d=identicon"
+
 @socketio.on("connect")
 def handle_connect():
     print("A user connected")
--- models/message.py
+++ models/message.py
@@ -1,5 +1,19 @@
-class Message:
-    def __init__(self, sender, content, timestamp):
-        self.sender = sender
-        self.content = content
-        self.timestamp = timestamp
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    avatar_url = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            "username": self.username,
+            "avatar_url": self.avatar_url,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%H:%M:%S")
+        }
